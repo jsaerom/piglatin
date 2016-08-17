b97f342479c49040c56604dfb60a1c1ec35c98d1
Joanna Anderson and Molly LeCompte <molly@>
2016-08-17 11:47:23 -0700

    Creates the first function for splitting input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,26 @@
-var inputPhrase = "";
-
 
 // Back end Logic
+var inputPhrase = "";
 var vowelArray = ["a", "e", "i", "o", "u", "A", "E",
 "I", "O", "U"];
 var consonantArray = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z", "B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Z"];
 var alphabetArray = vowelArray.concat(consonantArray);
+var splitPhrase = [];
 
-var test = function(input){
-var splitPhrase = inputPhrase.split("");
-};
+var splitSentence = function(input){
+  splitPhrase = input.split(" ");
+  for (var i = 0; i < splitPhrase.length; i++){
+  console.log(splitPhrase.slice(i, i+1));
+}
+
+}
+
+
 
 
 // Front end Logic
 $("form#pigLatin").submit(function(event) {
+  event.preventDefault();
   inputPhrase = $("#sentence").val();
-
-alert(test(inputPhrase));
-
-
+  splitSentence(inputPhrase);
 });
